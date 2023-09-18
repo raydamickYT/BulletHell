ea99b77e579625818b4708fe28cce6693ac77eae
Adam Bausch <Fireworksanimations21@gmail.com>
2023-09-18 20:47:03 +0200

    inputhandler werkts
    scaleable inputhandler aan de praat gekregen

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -3,43 +3,64 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEditor.VersionControl;
 using UnityEngine;
+using System;
 
 //dit is een class om de command pattern uit te voeren. de command pattern heeft altijd: een client, concrete command, abstract command, invoker en receiver.
 public interface ICommand
 {
     void Execute();
 }
-public interface IGameObjectCommand
-{
-    //opzetten van de concrete command
-    void Execute();
-}
 public class InputHandler
 {
-    private IGameObjectCommand Ycommand;
-    private IGameObjectCommand Xcommand;
-    public IGameObjectCommand Acommand;
-    public IGameObjectCommand Bcommand;
+    private List<KeyCommand> keyCommands = new List<KeyCommand>();
 
-    public InputHandler(IGameObjectCommand XButtonCommand, IGameObjectCommand YButtonCommand)
+    public void HandleInput()
     {
-        this.Xcommand = XButtonCommand;
-        this.Ycommand = YButtonCommand;
+        foreach (var keyCommand in keyCommands)
+        {
+            if (Input.GetKeyDown(keyCommand.key))
+            {
+                keyCommand.command.Execute();
+            }
+        }
     }
 
-    public IGameObjectCommand HandleInput()
+
+    //deze snap ik niet helemaal, vraag even in de les voor uitleg
+    public void BindInputToCommand(KeyCode keyCode, ICommand command)
     {
-        if (Input.GetKeyDown(KeyCode.Y)) { return Ycommand; }
-        if (Input.GetKeyDown(KeyCode.X)) { return Xcommand; }
-        // if (Input.GetKeyDown(KeyCode.A)) { Acommand.Execute(); };
-        // if (Input.GetKeyDown(KeyCode.B)) { Bcommand.Execute(); };
-        return null;
+        keyCommands.Add(new KeyCommand()
+        {
+            key = keyCode,
+            command = command
+        });
     }
+
+    public void UnBindInput(KeyCode keyCode)
+    {
+        var items = keyCommands.FindAll(x => x.key == keyCode);
+        items.ForEach(x => keyCommands.Remove(x));
+    }
+
+    // public IGameObjectCommand HandleInput()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Y)) { return Ycommand; }
+    //     if (Input.GetKeyDown(KeyCode.X)) { return Xcommand; }
+    //     // if (Input.GetKeyDown(KeyCode.A)) { Acommand.Execute(); };
+    //     // if (Input.GetKeyDown(KeyCode.B)) { Bcommand.Execute(); };
+    //     return null;
+    // }
+}
+
+public class KeyCommand
+{
+    public KeyCode key;
+    public ICommand command;
 }
 
 
 //concrete command
-public class FireGunCommand : IGameObjectCommand
+public class FireGunCommand : ICommand
 {
     //dit is de uitvoering van de concrete command
     public void Execute()
@@ -53,7 +74,7 @@ public class FireGunCommand : IGameObjectCommand
     }
 }
 
-public class TestMessage : IGameObjectCommand
+public class TestMessage : ICommand
 {
     public void Execute()
     {
@@ -67,22 +88,3 @@ public class TestMessage : IGameObjectCommand
     }
 }
 
-public class Test2 : MonoBehaviour
-{
-    public InputHandler inputHandler = new InputHandler(new FireGunCommand(), new TestMessage());
-    public IGameObjectCommand command;
-    public GameObject test;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        command = inputHandler.HandleInput();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        command.Execute();
-    }
-}
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -6,23 +6,32 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public InputHandler inputHandler;
-    public IGameObjectCommand command;
+    //public ICommand command;
     public GameObject test;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        inputHandler = new InputHandler(new FireGunCommand(), new TestMessage());
+        //setup de input handler op een game object.
+        inputHandler = new InputHandler();
+        //door een var aan te maken voor een van de acties, hoef je niet de hele tijd 'new firefuncommand()" te schrijven en kan je meerdere knoppen voor een action gebruiken.
+        var firegun = new FireGunCommand();
+        //deze line zorgt ervoor dat er een key wordt gebind aan de juiste actie. de actie zit op de actor
+        inputHandler.BindInputToCommand(KeyCode.X, firegun);
+        inputHandler.BindInputToCommand(KeyCode.Y, firegun);
+        inputHandler.BindInputToCommand(KeyCode.R, new TestMessage());
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        command = inputHandler.HandleInput();
+        inputHandler.HandleInput();
 
-        if (command != null)
-        {
-            command.Execute();
-        }
+        // if (command != null)
+        // {
+        //     command.Execute();
+        // }
     }
 }
